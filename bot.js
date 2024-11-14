985990455f2149145cc9d26dbf284456a1764f33
Gustavo Tocantins <64499085+gustavotocantins@users.noreply.github.com>
2024-11-14 20:31:06 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');  // Para manipulação de arquivos
+const puppeteer = require('puppeteer-core');
+const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
 
 let clientsInProgress = {};  // Armazena o estado de cada cliente
 
