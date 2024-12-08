7806b0200c6e3a4c510d2ed660192842b356cc07
Gustavo Tocantins <64499085+gustavotocantins@users.noreply.github.com>
2024-12-08 18:45:16 -0300

    Update agua.js

--- BotAgua/agua.js
+++ BotAgua/agua.js
@@ -2,7 +2,93 @@ const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');  // Para manipulação de arquivos
 const axios = require('axios');
+const express = require('express');
 const path = require('path');
+const { Payment, MercadoPagoConfig } = require("mercadopago");
+const clientPIX = new MercadoPagoConfig({ accessToken: 'APP_USR-7005128537550780-120100-27ba39bde39862bd3558b846cd618f5d-696187036', options: { timeout: 5000, idempotencyKey: 'abc' } });
+const bodyParser = require('body-parser');
+
+// Configurando o bot para enviar mensagem
+
+// Configura o servidor Express
+const app = express();
+app.use(bodyParser.json()); // Para trabalhar com requisições JSON
+
+// Endpoint genérico para enviar mensagens
+app.post('/enviar-mensagem', async (req, res) => {
+    const { numero, mensagem } = req.body;
+
+    if (!numero || !mensagem) {
+        return res.status(400).send({ error: 'Parâmetros "numero" e "mensagem" são obrigatórios!' });
+    }
+
+    try {
+        // Envia a mensagem para o número especificado
+        await client.sendMessage(
+            `${numero}@c.us`,
+            mensagem
+        );
+        console.log(`Mensagem enviada para ${numero}`);
+
+        res.status(200).send({ message: 'Mensagem enviada com sucesso!' });
+    } catch (error) {
+        console.error('Erro ao enviar mensagem:', error);
+        res.status(500).send({ error: 'Erro ao enviar mensagem' });
+    }
+
+});
+
+// Endpoint para verificar se o servidor está ativo
+app.get('/', (req, res) => {
+    res.send('Servidor está ativo e rodando!');
+});
+
+// Inicializa o servidor na porta 3000
+const PORT = 3010;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+// Step 3: Initialize the API object
+const payment = new Payment(clientPIX);
+async function criarPedido({ valor, informa, idCompra}) {
+    // Criar o corpo da requisição
+    const email = 'gustavotocantins@gmail.com';
+    const body = {
+        transaction_amount:valor,
+        description:informa,
+        payment_method_id: 'pix', // Mantém fixo como PIX
+        payer: {
+            email,
+        },
+    };
+
+    // Opções da requisição (opcional)
+    const requestOptions = {
+        idempotencyKey: `${idCompra}`, // Gera uma chave única baseada no timestamp
+    };
+    
+    try {
+        // Fazendo a requisição para criar o pedido
+        const pedido = await payment.create({ body, requestOptions });
+
+        
+        // Acessando e exibindo o QR Code
+        const qrCode = pedido?.point_of_interaction?.transaction_data?.qr_code;
+		
+
+        if (!qrCode) {
+            console.error("QR Code não encontrado no objeto retornado.");
+        } else {
+            console.log("QR Code:", qrCode);
+			console.log("Status:", pedido.status);
+			return {chave:qrCode,status:pedido.status}
+        }
+    } catch (error) {
+        // Tratamento de erros
+        console.error("Erro ao criar o pedido ou acessar propriedades:", error.message);
+    }
+}
 
 let clientsInProgress = {};  // Armazena o estado de cada cliente
 let clientTimers = {};  // Armazena os temporizadores para cada cliente
@@ -13,8 +99,10 @@ const client = new Client({
     puppeteer: {
         executablePath: '/usr/bin/chromium-browser',
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        timeout: 60000,
     }, 
 });
+
 
 // Gera o QR code para login no WhatsApp Web
 client.on('qr', (qr) => {
@@ -69,10 +157,10 @@ client.on('message', (message) => {
         console.log(message.body);
 
         if (!clientsInProgress[message.from]) {
-            if (!isWithinWorkingHours()) {
+            if (false) {
                 message.reply('Desculpe, nosso horário de atendimento é das 8h às 18h. Por favor, entre em contato novamente mais tarde.');
                 delete clientsInProgress[message.from];
-                return;
+                return;//!isWithinWorkingHours()
             }
             // Marca o cliente como em atendimento e envia a mensagem de boas-vindas
             clientsInProgress[message.from] = {tipoAgua: '',
@@ -127,24 +215,40 @@ const numeroTipo = {
     'dois': 2,
     'três': 3,
     'tres': 3,
+    '1':1,
+    '2':2,
+    '3':3,
     '10':1,
     '12':2,
     '15':3,
     'nossa agua':1,
     'agua cristal':2,
-    'indaia':3
+    'indaia':3,
+    'dez':1,
+    'doze':2,
+    'quinze':3,
+
   };
 // Função para gerenciar a resposta do cliente
 async function handleClientResponse(client, message) {
     const pedido = clientsInProgress[message.from];
-
+    const produtoIDs = {
+        'Nossa Água': 1,
+        'Água Cristal': 2,
+        'Indaiá': 3
+    };
+    
+    function GerarIDpagamento() {
+        // Gera um número entre 10000000 e 99999999 (8 dígitos)
+        return Math.floor(10000000 + Math.random() * 90000000);
+    }
     // Reinicia o temporizador sempre que o cliente interage
     resetInactivityTimer(message.from);
     // Quando o cliente enviar "Oi", "Olá" ou qualquer saudação
 
     // Se o cliente responder "Sim" ou "Não"
     if (pedido.estado === 'iniciar' && Confirmo.test(message.body.toLowerCase())) {
-        let options = aguas.map((agua, index) => `${index + 1}. ${agua.nome} - Valor: R$ ${agua.preco}`).join('\n');
+        let options = aguas.map((agua, index) => `${index + 1}️⃣ ${agua.nome} (R$ ${agua.preco})`).join('\n');
         message.reply('Ótimo! Vamos começar. Escolha o *tipo de água* que deseja:')
         .then(() => client.sendMessage(message.from,options));
         pedido.estado = 'escolherMarca'; // Definir estado para escolha da marca
@@ -152,7 +256,7 @@ async function handleClientResponse(client, message) {
 
     else if (message.body.toLowerCase() === 'não' && pedido.estado === 'iniciar') {
         message.reply('Poxa, que pena que não vai precisar de água. Mas qualquer coisa, estamos aqui. Desde já, agradecemos o contato!');
-        delete clientsInProgress[clientId]; // Resetar o estado
+        delete clientsInProgress[message.from]; // Resetar o estado
     }
 
     // Se o cliente escolheu a marca da água
@@ -162,7 +266,6 @@ async function handleClientResponse(client, message) {
         const palavrasChave = Object.keys(numeroTipo);
         const regex = new RegExp(`\\b(${palavrasChave.join('|')})\\b`, 'gi');
         const encontrados = escolha.match(regex);
-        console.log(encontrados[0]);
         console.log('Encontrados');
         let escolhaNumero
         try{
@@ -188,7 +291,7 @@ async function handleClientResponse(client, message) {
             message.reply(`Quantas *unidades* de 20 litros você gostaria de pedir?`);
             pedido.estado = 'quantidade'; // Passar para o próximo passo
         } else {
-            message.reply('Escolha inválida. Por favor, escolha um número de 1 a 3 ou digite o nome do tipo de água.');
+            message.reply('Opa! Para prosseguir você pode escolher um número de 1 a 3 ou digitar o nome do tipo de água.');
         }
     }
 }
@@ -220,14 +323,7 @@ async function handleClientResponse(client, message) {
     // Receber o nome
     else if (pedido.estado === 'nome') {
         pedido.nome = message.body;
-        message.reply(`Olá, *${pedido.nome}!* Por favor, me informe seu *telefone* para contato.`);
-        pedido.estado = 'telefone'; // Passar para o próximo passo
-    }
-
-    // Receber o telefone
-    else if (pedido.estado === 'telefone') {
-        pedido.telefone = message.body;
-        message.reply('Agora, por favor, me informe o *seu endereço* para *entrega.*');
+        message.reply(`Olá, *${pedido.nome}!* Por favor, me informe seu *endereço* para entrega.`);
         pedido.estado = 'endereco'; // Passar para o próximo passo
     }
 
@@ -235,39 +331,67 @@ async function handleClientResponse(client, message) {
     else if (pedido.estado === 'endereco') {
         pedido.endereco = message.body;
         message.reply(`Você informou o seguinte endereço: ${pedido.endereco}.`)
-        .then(() => client.sendMessage(message.from,'Vamos verificar a taxa de entrega. Mande a sua *localização fixa:*\n*Android:* clique no clip 📎, selecione localização e escolha a opção “localização atual”.\n*iPhone:* clique no ➕, selecione localização e escolha a opção “localização atual”.'));
+        .then(() => client.sendMessage(message.from,'Vamos verificar a taxa de entrega. Mande a sua *localização fixa ou nome do seu bairro:*\n*Android:* clique no clip 📎, selecione localização e escolha a opção “localização atual”.\n*iPhone:* clique no ➕, selecione localização e escolha a opção “localização atual”.'));
         pedido.estado = 'taxa'; // Passar para o próximo passo
     }
-    else if(message.location && pedido.estado === 'taxa'){
-        const { latitude, longitude } = message.location;
-        const bairro = await getBairro(latitude, longitude);
+    else if(message.location || pedido.estado === 'taxa'){
+        let bairro = null;
+
+    try {
+        // Se a localização está disponível, tenta extrair o bairro
+        if (message.location) {
+            const { latitude, longitude } = message.location;
+            bairro = await getBairro(latitude, longitude); // Função para identificar bairro via API
+        }
+    } catch (error) {
+        console.error("Erro ao obter bairro pela localização:", error);
+    }
+
+    if (!bairro) {
+        // Tenta identificar o bairro a partir do texto da mensagem
+        const descobrirBairro = message.body.toLowerCase();
+        const bairrosConhecidos = ['campina', 'umarizal', 'jurunas', 'nazaré', 'reduto', 'cidade velha'];
+        const bairroEncontrado = bairrosConhecidos.find(b => descobrirBairro.includes(b));
+
+        // Define o bairro encontrado ou "Sem Bairro" se nenhum foi identificado
+        bairro = bairroEncontrado ? bairroEncontrado.charAt(0).toUpperCase() + bairroEncontrado.slice(1) : 'Sem Bairro';
+        console.log("Bairro identificado pela mensagem:", bairro);
+    }
             if (bairro === "Campina" || bairro === "Cidade Velha"){
                 client.sendMessage(message.from, `A entrega para o bairro Campina é *gratuita!*`)
                 .then(() => client.sendMessage(message.from,`Qual será a forma de pagamento? Responda *"Dinheiro"* ou *"Pix"*.`));
+                pedido.bairro = "Campina"
                 pedido.taxa = 0;
             } else if(bairro == "Umarizal"){
                 client.sendMessage(message.from, `A taxa de entrega para o bairro *${bairro}* é de R$ 15,00`)
                 .then(() => client.sendMessage(message.from,`Qual será a forma de pagamento? Responda *"Dinheiro"* ou *"Pix"*.`));
                 pedido.taxa = 15;
+                pedido.bairro = "Umarizal"
             } else if(bairro == 'Jurunas'){
                 client.sendMessage(message.from, `A taxa de entrega para o bairro *${bairro}* é de R$ 14,80`)
                 .then(() => client.sendMessage(message.from,`Qual será a forma de pagamento? Responda *"Dinheiro"* ou *"Pix"*.`));
                 pedido.taxa = 14.8;
+                pedido.bairro = "Jurunas"
             }else if(bairro == 'Nazaré'){
                 client.sendMessage(message.from, `A taxa de entrega para o bairro *${bairro}* é de R$ 16,50`)
                 .then(() => client.sendMessage(message.from,`Qual será a forma de pagamento? Responda *"Dinheiro"* ou *"Pix"*.`));
                 pedido.taxa = 16.5;
+                pedido.bairro = "Nazaré"
             }else if(bairro == 'Reduto'){
                 client.sendMessage(message.from, `A taxa de entrega para o bairro *${bairro}* é de R$ 8,50`)
                 .then(() => client.sendMessage(message.from,`Qual será a forma de pagamento? Responda *"Dinheiro"* ou *"Pix"*.`));
                 pedido.taxa = 8.5;
+                pedido.bairro = "Reduto"
+            }else if(bairro == 'Sem Bairro'){
+                client.sendMessage(message.from, `Poxa, sentimos muito! Ainda não estamos atendendo no seu bairro, mas esperamos chegar aí em breve!`)
+                delete clientsInProgress[message.from];
             } else{
                 client.sendMessage(message.from, `Poxa, sentimos muito! Ainda não estamos atendendo o bairro *${bairro},* mas esperamos chegar aí em breve!`);
                 delete clientsInProgress[message.from];
                 return
             }
         pedido.estado = 'pagamento';
-    }
+}
     // Receber a forma de pagamento
     else if (pedido.estado === 'pagamento') {
         var_dinheiro = ['dinheiro','real','grana','fisico'];
@@ -279,12 +403,59 @@ async function handleClientResponse(client, message) {
             pedido.pagamento = 'Dinheiro';
             message.reply('Você escolheu pagamento em dinheiro. Precisa de troco?');
             pedido.estado = 'troco'; // Passar para o próximo passo
-        } else if (dinheiro.test(message.body.toLowerCase())) {
+        } else if (pix.test(message.body.toLowerCase())) {
             pedido.pagamento = 'Pix';
-            message.reply('Você escolheu pagamento via Pix.\n Nossa *chave pix* é 04588776374')
-            .then(() => client.sendMessage(message.from,'Agora vou passar o resumo do seu pedido. OK?'));
+            pedido.IdPix =  GerarIDpagamento();
+            (async () => {
+                try {
+                  
+                    const ComandoPix = await criarPedido({
+                        valor: 0.5, //pedido.quantidade * pedido.preco + pedido.taxa
+                        description: `Tipo de água: _${pedido.tipoAgua}_\nQuantidade: _${pedido.quantidade} garrafões de 20L_\nPreço unitário: _R$ ${pedido.preco}_\n`, // Descrição do produto
+                        idCompra: pedido.IdPix, // E-mail ou ID do pagador
+                    });
+            
+                    // Mensagem de resposta ao usuário
+                    await message.reply(`Você escolheu pagamento via *Pix.*\n_Copie a *chave-pix* e pague no seu banco ⤵️_`);
+
+                    // Enviando o status do Pix
+                    await client.sendMessage(message.from, `${ComandoPix.chave}`);
+                } catch (error) {
+                    console.error("Erro ao criar o Pix:", error);
+                    await client.sendMessage(message.from, "Ocorreu um erro ao processar seu pedido. Tente novamente mais tarde.");
+                    delete pedido.IdPix;
+                }
+            })();
+
+            const interval = await setInterval(async () => {
+                const EstadoPagamento = await criarPedido({
+                    valor: 0.5, //pedido.quantidade * pedido.preco + pedido.taxa
+                    description: `Tipo de água: _${pedido.tipoAgua}_\nQuantidade: _${pedido.quantidade} garrafões de 20L_\nPreço unitário: _R$ ${pedido.preco}_\n`, // Descrição do produto
+                    idCompra: pedido.IdPix, // E-mail do pagador
+                });
+
+                if (EstadoPagamento.status === 'approved') {
+                    clearInterval(interval); // Parar verificações
+                    clearTimeout(timeout); // Cancelar o timeout final
+                    client.sendMessage(message.from,'Pagamento aprovado✅!\nDigite *"OK"* para finalizar!');
+                    delete pedido.IdPix;
+                    pedido.estado = 'resumo';
+                } else {
+                    console.log("Aguardando pagamento...");
+                }
+            }, 6000); // Verifica a cada 4 segundos
+            // Timeout para cancelar após 1 minuto
+            const timeout = setTimeout(() => {
+                clearInterval(interval); // Parar verificações
+                if (pedido.estado !== 'Pago') {
+                    client.sendMessage(message.from,'Seu tempo para pagar, acabou! Deseja pagar no dinheiro ou no pix?');
+                    delete pedido.IdPix;
+                    pedido.estado = 'pagamento';
+                }
+            }, 120000); // 1 minuto (60000 ms)
+           
             pedido.troco = 0;
-            pedido.estado = 'resumo'; // Passar para o próximo passo
+            
         } else {
             message.reply('Desculpe, não entendi. Responda "Dinheiro" ou "Pix".');
         }
@@ -329,7 +500,7 @@ async function handleClientResponse(client, message) {
         mensagemResumo += `*Total: _R$ ${total}_*\n`;
         mensagemResumo += `*🚘 DADOS DE ENTREGA*\n`;
         mensagemResumo += `Nome: _${pedido.nome}_\n`;
-        mensagemResumo += `Telefone: _${pedido.telefone}_\n`;
+        mensagemResumo += `Telefone: _${message.from.split('@')[0]}_\n`;
         mensagemResumo += `Endereço: _${pedido.endereco}_\n`;
         mensagemResumo += `Forma de pagamento: _${pedido.pagamento}_\n`;
 
@@ -341,9 +512,48 @@ async function handleClientResponse(client, message) {
         }
         message.reply(mensagemResumo)
         .then(() => client.sendMessage(message.from,'Seu pedido *foi concluído!✅* Entraremos em contato em breve para a entrega. Obrigado!'));
+        const dadosPedido = {
+            itens: `[${pedido.tipoAgua}]`,
+            local: `${pedido.bairro}`,
+            valor: total,
+            pagamento: pedido.pagamento,
+            descricao: `${pedido.quantidade}x ${pedido.tipoAgua}`,
+            cliente: pedido.nome,
+            whatsapp: message.from.split('@')[0]
+          };
+          
+          axios.post('http://localhost:3001/pedido', dadosPedido)
+        .then(response => {
+            console.log('Pedido adicionado com sucesso:', response.data);
+
+            // Reduzir quantidade no estoque
+            const produtoID = produtoIDs[pedido.tipoAgua];
+            if (produtoID) {
+                axios.get(`http://localhost:3001/estoque/${produtoID}`)
+                    .then(({ data }) => {
+                        const novaQuantidade = data.produto.quantidade - pedido.quantidade;
+
+                        // Atualizar estoque com a nova quantidade
+                        if (novaQuantidade >= 0) {
+                            axios.put(`http://localhost:3001/estoque/${produtoID}`, { quantidade: novaQuantidade })
+                                .then(() => console.log(`Estoque do produto ID ${produtoID} atualizado com sucesso.`))
+                                .catch(err => console.error('Erro ao atualizar estoque:', err.response ? err.response.data : err.message));
+                        } else {
+                            console.warn(`Quantidade insuficiente no estoque para o produto ID ${produtoID}.`);
+                        }
+                    })
+                    .catch(err => console.error('Erro ao buscar produto:', err.response ? err.response.data : err.message));
+            } else {
+                console.warn('Produto não encontrado no mapeamento de IDs.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao adicionar pedido:', error.response ? error.response.data : error.message);
+        });
 
         // Enviar Pedido para o contato pessoal do dono
         client.sendMessage('559192431116@c.us', `Novo Pedido✅ ${mensagemResumo}`);
+        //pedido.estado = "FimPedido"
         delete clientsInProgress[message.from]; // Resetar o pedido para iniciar um novo atendimento
     }
 }
@@ -355,7 +565,7 @@ function startInactivityTimer(clientId) {
         // Se não houver interação por 5 minutos, reinicia o atendimento
         console.log(`Cliente ${clientId} inativo por 5 minutos. Reiniciando atendimento...`);
         resetClientState(clientId);
-    }, 5 * 60 * 1000); // 5 minutos
+    }, 45 * 60 * 1000); // 5 minutos
 }
 
 // Função para resetar o temporizador de inatividade (sempre que o cliente interagir)
